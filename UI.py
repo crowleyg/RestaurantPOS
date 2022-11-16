0f3c5481bc5d1715e3bdb5b6fc6bfd467d824331
crowleyg <crowleyg@oregonstate.edu>
2022-11-16 09:44:25 -0800

    updated all project files to near completion

--- UI.py
+++ UI.py
@@ -1,12 +1,114 @@
 import PySimpleGUI as sg
 import time
 
+# set color theme
 sg.theme('Purple')
 
+# dropdown options
 table_list = ["Table1", "Table2", "Table3", "Table4", "Table5", "Table6"]
-
 add_remove = ["Add Item", "Remove Item"]
 
+# column layouts
+col1 = [[sg.Text("Current Ticket", font=('Arial', 15, 'bold', 'underline'))],
+          [sg.Multiline(size=(25, 8), key='-ITEM_ORDERS-')],
+          [sg.Button("Total Ticket",tooltip=("Calculate totals, tax and gratuity")), sg.Button("Clear Ticket",tooltip=("Clear entire ticket for current table"))]]
+col2 = [[sg.Text("                          Subtotal:"), sg.Multiline(size=(10, 2), key='-SUBTOTAL-')],
+          [sg.Text("                                 Tax:"), sg.Multiline(size=(10, 2), key='-TAX-')],
+          [sg.Text("                        TOTAL:", font=('Arial', 12, 'bold',)), sg.Multiline(size=(10, 2), key='-TOTAL-')],
+          [sg.Text("Suggested Gratuities\n              (10%,15%,20%):"), sg.Multiline(size=(10, 4), key='-GRATUITY-')]]
+
+# constants
+TAX_RATE = .0725
+
+# function definitions
+def clear_totals():
+    """ Clear Total Output Windows"""
+    window['-SUBTOTAL-'].update('')
+    window['-TAX-'].update('')
+    window['-TOTAL-'].update('')
+    window['-GRATUITY-'].update('')
+
+def display_order():
+    """ Update Window to Current Ticket When User Selects New Table"""
+    file = open(f"CurrentTickets\\{values['-TABLE-']}_Food.txt", "r")
+    file2 = open(f"CurrentTickets\\{values['-TABLE-']}_Tab.txt", "r")
+    window['-ITEM_ORDERS-'].update('')
+    for line, line2 in zip(file, file2):
+        window['-ITEM_ORDERS-'].print(line, line2)
+    file.close()
+    file2.close()
+
+def retrieve_price():
+    """ Retrieve item price"""
+    file = open(f"FoodItems\\{event}.txt", "r")
+    price = file.readline()
+    file.close()
+    return price
+
+def add_item(price):
+    """ Add item to the food and tab files"""
+    with open(f"CurrentTickets\\{values['-TABLE-']}_Food.txt", "a") as food:
+        food.write(f'{event}\n')
+    with open(f"CurrentTickets\\{values['-TABLE-']}_Tab.txt", "a") as tab:
+        tab.write(f'{price}\n')
+
+def remove_item():
+    """ Remove item from the food and tab files"""
+    food = open(f"CurrentTickets\\{values['-TABLE-']}_Food.txt", "r+")
+    food_lines = food.readlines()
+    tab = open(f"CurrentTickets\\{values['-TABLE-']}_Tab.txt", "r+")
+    tab_lines = tab.readlines()
+    food.truncate(0)
+    tab.truncate(0)
+    food.close()
+    tab.close()
+
+    food = open(f"CurrentTickets\\{values['-TABLE-']}_Food.txt", "r+")
+    tab = open(f"CurrentTickets\\{values['-TABLE-']}_Tab.txt", "r+")
+    found = 0
+    for line1, line2 in zip(food_lines, tab_lines):
+        if line1.strip("\n") != event or found == 1:
+            food.write(line1)
+            tab.write(line2)
+        else:
+            found = 1
+    food.close()
+    tab.close()
+
+def calculate_subtotal():
+    """ Add item prices to calculate subtotal"""
+    tab = open(f"CurrentTickets\\{values['-TABLE-']}_Tab.txt", "r+")
+    tab_lines = tab.readlines()
+    tab.close()
+
+    subtotal = 0
+    for line in tab_lines:
+        line = line.rstrip()
+        subtotal += float(line)
+    return subtotal
+
+def call_gratuity_microservice():
+    """ Update Gratuity Window With Gratuity Microservice Call"""
+    total_file = open("total.txt", "w")
+    total_file.truncate(0)  # prevents invalid entry
+    total_file.write(f"{total}")
+    total_file.close()
+    time.sleep(1)
+
+    with open("gratuity.txt", "r") as grat:
+        for line in grat:
+            window['-GRATUITY-'].print(line.strip("\n"))
+
+def clear_ticket():
+    """ Clear the Currently Selected Table's Ticket"""
+    food = open(f"CurrentTickets\\{values['-TABLE-']}_Food.txt", "r+")
+    tab = open(f"CurrentTickets\\{values['-TABLE-']}_Tab.txt", "r+")
+    food.truncate(0)
+    tab.truncate(0)
+    food.close()
+    tab.close()
+
+# window layout
 layout = [[sg.Push(), sg.Button("What's New?",tooltip=("Displays new features")), sg.Button("How To Use",tooltip=("Displays instructions"))],
           [sg.Text("Table Number", font=('Arial', 15, 'bold', 'underline'))],
           [sg.DropDown(table_list, size=(10, 8), default_value=table_list[0], key="-TABLE-",tooltip=("Select Table Number")), sg.Button("Select Table",tooltip=("Change current table"))],
@@ -20,17 +122,12 @@ layout = [[sg.Push(), sg.Button("What's New?",tooltip=("Displays new features"))
           [sg.Text("Drinks")],
           [sg.Button("Coke",tooltip=("16oz Coke\n Price: $3.00 ")), sg.Button("Sprite",tooltip=("16oz Sprite\n Price: $3.00 ")), sg.Button("Lemonade",tooltip=("16oz Lemonade\n Price: $3.00 ")), sg.Button("Tea",tooltip=("16oz Tea\n Price: $3.00 "))],
           [sg.HorizontalSeparator()],
-          [sg.Text("Current Ticket", font=('Arial', 15, 'bold', 'underline'))],
-          [sg.Multiline(size=(25, 8), key='-ITEM_ORDERS-')],
-          [sg.Button("Total Ticket",tooltip=("Calculate totals, tax and gratuity")), sg.Button("Clear Ticket",tooltip=("Clear entire ticket for current table"))],
-          [sg.Text("")],
-          [sg.Text("                          Subtotal:"), sg.Multiline(size=(10, 1), key='-SUBTOTAL-')],
-          [sg.Text("                                 Tax:"), sg.Multiline(size=(10, 1), key='-TAX-')],
-          [sg.Text("                        TOTAL:", font=('Arial', 12, 'bold',)), sg.Multiline(size=(10, 1), key='-TOTAL-')],
-          [sg.Text("Suggested Gratuity (15%):"), sg.Multiline(size=(10,1), key='-GRATUITY-')]]
+          [sg.Column(col1, element_justification='left'),sg.Column(col2, element_justification='left')]]
 
-window = sg.Window('Restaurant POS System', layout, size=(600, 800), use_custom_titlebar=True)
+# create PySimpleGUI window
+window = sg.Window('Restaurant POS System', layout, size=(600, 600), use_custom_titlebar=True)
 
+# event loop
 while True:
     event, values = window.read()
 
@@ -38,11 +135,16 @@ while True:
         break
 
     if event == "What's New?":
-        choice, _ = sg.Window("What's New?", [[sg.Text("You no longer need to clear tickets one item at a time!")],
-                                              [sg.Text("Press the 'Clear Ticket' button to clear the entire ticket for a table.")],
+
+        # display text to 'What's New?' Window
+        choice, _ = sg.Window("What's New?", [[sg.Text("You no longer need to clear ticket items one at a time!")],
+                                              [sg.Text("To save time, press the 'Clear Ticket' button to clear the entire ticket for a table.")],
+                                              [sg.Text("*NOTE: 'Clear Ticket' removes ALL items on ticket. To remove one item, try 'Remove Item' instead.")],
                                               [sg.Push(), sg.Ok(s=10)]], disable_close=True).read(close=True)
 
     if event == "How To Use":
+
+        # display text to 'How To Use' Window
         choice, _ = sg.Window("How To Use", [[sg.Text("* Change Table Number to table currently being served.")],
                                              [sg.Text("* Click 'Select Table' to populate current ticket.")],
                                              [sg.HorizontalSeparator()],
@@ -56,107 +158,49 @@ while True:
                                              [sg.Push(), sg.Ok(s=10)]], disable_close=True).read(close=True)
 
     if event == "Select Table":
-        file = open(f"CurrentTickets\\{values['-TABLE-']}_Food.txt", "r")
-        file2 = open(f"CurrentTickets\\{values['-TABLE-']}_Tab.txt", "r")
-        window['-ITEM_ORDERS-'].update('')
-        for line, line2 in zip(file, file2):
-            window['-ITEM_ORDERS-'].print(line, line2)
 
-        #close the files
-        file.close()
-        file2.close()
-
-        #sg.Popup(event, values['-TABLE-'], values['-ADD_REMOVE-'])
+        display_order()
+        clear_totals()
 
     if (event == "ClassicBurger" or event == "Cheeseburger" or event == "ChickenSandwich" or event == "FrenchDip"
         or event == "Fries" or event == "OnionRings" or event == "SweetPotatoFries" or event == "FriedZucchini"
         or event == "Coke" or event == "Sprite" or event == "Lemonade" or event == "Tea"):
 
-        file = open(f"FoodItems\\{event}.txt", "r")
-        line = file.readline()
-        file.close()
+        price = retrieve_price()
 
-        # Adding items to ticket
         if values['-ADD_REMOVE-'] == "Add Item":
+            add_item(price)
+            display_order()
 
-            # add the item to the food and tab files
-            with open(f"CurrentTickets\\{values['-TABLE-']}_Food.txt", "a") as food:
-                food.write(f'{event}\n')
-            with open(f"CurrentTickets\\{values['-TABLE-']}_Tab.txt", "a") as tab:
-                tab.write(f'{line}\n')
-
-
-            # update the current ticket window
-            window['-ITEM_ORDERS-'].update('')
-            food = open(f"CurrentTickets\\{values['-TABLE-']}_Food.txt", "r")
-            tab = open(f"CurrentTickets\\{values['-TABLE-']}_Tab.txt", "r")
-            for line1, line2 in zip(food, tab):
-                window['-ITEM_ORDERS-'].print(line1, line2)
-            food.close()
-            tab.close()
-
-        #removing items from ticket
+        # removing items from ticket
         if values['-ADD_REMOVE-'] == "Remove Item":
-            food = open(f"CurrentTickets\\{values['-TABLE-']}_Food.txt", "r+")
-            food_lines = food.readlines()
-            tab = open(f"CurrentTickets\\{values['-TABLE-']}_Tab.txt", "r+")
-            tab_lines = tab.readlines()
+            remove_item()
+            display_order()
 
-            food.truncate(0)
-            tab.truncate(0)
-            food.close()
-            tab.close()
-            food = open(f"CurrentTickets\\{values['-TABLE-']}_Food.txt", "r+")
-            tab = open(f"CurrentTickets\\{values['-TABLE-']}_Tab.txt", "r+")
-            found = 0
-            for line1, line2 in zip(food_lines, tab_lines):
-                if line1.strip("\n") != event or found == 1:
-                    food.write(line1)
-                    tab.write(line2)
-                else:
-                    found = 1
-            food.close()
-            tab.close()
+    if event == "Total Ticket":
 
-            # update the current ticket window
-            window['-ITEM_ORDERS-'].update('')
-            food = open(f"CurrentTickets\\{values['-TABLE-']}_Food.txt", "r")
-            tab = open(f"CurrentTickets\\{values['-TABLE-']}_Tab.txt", "r")
-            for line1, line2 in zip(food, tab):
-                window['-ITEM_ORDERS-'].print(line1, line2)
-            food.close()
-            tab.close()
+        # calculate totals
+        subtotal = calculate_subtotal()
+        tax = round(subtotal * TAX_RATE, 2)
+        total = subtotal + tax
 
-            #for count, line1, line2 in enumerate(zip(food, tab)):
-                #if line1 == event:
-
-
+        # update total windows
+        clear_totals()
+        window['-SUBTOTAL-'].print(subtotal)
+        window['-TAX-'].print(tax)
+        window['-TOTAL-'].print(total)
+        call_gratuity_microservice()
 
     if event == "Clear Ticket":
-        # verify user wants to clear
+
+        # verify user wants to clear current ticket
         choice, _ = sg.Window('Continue?',[[sg.Text("Are you sure you want to clear the ticket?")],[sg.Yes(s=10), sg.No(s=10)]], disable_close=True).read(close=True)
         if choice == "Yes":
-
-            # clear the files
-            food = open(f"CurrentTickets\\{values['-TABLE-']}_Food.txt", "r+")
-            tab = open(f"CurrentTickets\\{values['-TABLE-']}_Tab.txt", "r+")
-            food.truncate(0)
-            tab.truncate(0)
-            food.close()
-            tab.close()
-
-            #update the current ticket window
-            window['-ITEM_ORDERS-'].update('')
-            food = open(f"CurrentTickets\\{values['-TABLE-']}_Food.txt", "r")
-            tab = open(f"CurrentTickets\\{values['-TABLE-']}_Tab.txt", "r")
-            for line1, line2 in zip(food, tab):
-                window['-ITEM_ORDERS-'].print(line1, line2)
-            food.close()
-            tab.close()
+            clear_ticket()
+            display_order()
+            clear_totals()
 
             # display confirmation
             sg.popup('Ticket Cleared')
-
-#for tax and total use window[key].update(line)
 
 window.close()
